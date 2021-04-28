5683ad062ce203057a27402e94000fd33a2e4a7e
Bung Danar <danar.g.putera@gmail.com>
2021-04-28 10:47:13 +0700

    chore: Add basic request handler

---
+++ src/utils/handle-request.ts
@@ -0,0 +1,29 @@
+import axios from 'axios'
+import qs from 'qs'
+import { handleResponseErr } from './handle-response-err'
+
+const SERVER_BASE_URL =
+  process.env.NODE_ENV === 'production'
+    ? process.env.REACT_APP_BACKEND_API_PROD
+    : process.env.REACT_APP_BACKEND_API
+
+export const request = {
+  get: <T>(url: string, params: Object = {}, additionalConfig: Object = {}) =>
+    axios
+      .get<T>(`${SERVER_BASE_URL}${url}`, {
+        params,
+        paramsSerializer: (params) =>
+          qs.stringify(params, { arrayFormat: 'repeat' }),
+        withCredentials: true,
+        ...additionalConfig,
+      })
+      .catch((err) => handleResponseErr(err)),
+
+  post: <T>(url: string, payload: Object = {}, additionalConfig: Object = {}) =>
+    axios
+      .post<T>(`${SERVER_BASE_URL}${url}`, payload, {
+        withCredentials: true,
+        ...additionalConfig,
+      })
+      .catch((err) => handleResponseErr(err)),
+}
