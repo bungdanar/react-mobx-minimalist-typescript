675e3bfd68fbf4c964335245f06c0f3171fba4d0
Bung Danar <danar.g.putera@gmail.com>
2021-04-29 09:43:39 +0700

    chore: Add session timeout component

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 import { observer } from 'mobx-react-lite'
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
+import moment from 'moment'
 import { StoreContext } from '../../contexts/store-context'
 import {
   errorSwal,
@@ -31,11 +32,46 @@ const Navbar = observer(() => {
       <div className='navbar-brand'>Navbar</div>
       <div className='d-flex justify-content-end '>
         <div className='navbar-brand'>{currentUser?.name}</div>
+        <SessionCountdown />
         <div className={logoutStyles.join(' ')} onClick={handleLogout}>
           Logout
         </div>
       </div>
     </nav>
+  )
+})
+
+const SessionCountdown = observer(() => {
+  const { currentUser } = useContext(StoreContext).userStore
+  const [timeLeft, setTimeLeft] = useState(0)
+
+  useEffect(() => {
+    if (currentUser) {
+      const findTimeLeft = () => {
+        const now = Math.floor(Date.now() / 1000)
+        const differ = currentUser.exp! - now
+
+        setTimeLeft(differ)
+      }
+
+      findTimeLeft()
+
+      const timerId = setInterval(findTimeLeft, 1000)
+
+      return () => {
+        clearInterval(timerId)
+      }
+    }
+  }, [currentUser])
+
+  return (
+    <div className='navbar-brand'>
+      {timeLeft < 0
+        ? 'Sess expired'
+        : `${moment.duration(timeLeft, 'seconds').hours()}:${moment
+            .duration(timeLeft, 'seconds')
+            .minutes()}:${moment.duration(timeLeft, 'seconds').seconds()}`}
+    </div>
   )
 })
 
