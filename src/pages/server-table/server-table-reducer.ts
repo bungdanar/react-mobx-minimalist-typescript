4e1f048d0c9a9e0db3c93878365a2fdb6801f1d2
Bung Danar <danar.g.putera@gmail.com>
2021-04-30 16:45:24 +0700

    chore: Set up server table page

---
+++ src/pages/server-table/server-table-reducer.ts
@@ -0,0 +1,13 @@
+import { User } from '../../data-types/user'
+
+type ServerTableState = {
+  data: User[]
+  loading: boolean
+  errMessage: string
+  pageCount: number
+  rowCount: number
+}
+
+// const serverTable: ServerTableState = {
+
+// }
