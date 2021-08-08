4dba9ad982367788a7a06a6bae91e76542735c0c
Bung Danar <danar.g.putera@gmail.com>
2021-08-08 21:21:53 +0700

    chore: Improve table pagination style

--- src/components/custom-table/table-pagination/TablePagination.tsx
+++ src/components/custom-table/table-pagination/TablePagination.tsx
@@ -24,44 +24,52 @@ export default function TablePagination({
   isLoading = false,
 }: TablePaginationProps): JSX.Element {
   return (
-    <div className={styles.tablePagination}>
-      <div className={styles.verticalCenterText}>
-        Page {pageIndex + 1} of {pageOptions.length}
-      </div>
-      <div className={styles.paginationSection}>
-        <div className='input-group'>
-          <div className={styles.verticalCenterText}>
-            <span>Go to Page</span>
+    <>
+      <div className={['row', styles.tablePagination].join(' ')}>
+        <div className='col'>
+          <span className='align-middle'>
+            Page {pageIndex + 1} of {pageOptions.length}
+          </span>
+        </div>
+        <div className='col'>
+          <div className='input-group'>
+            <div>
+              <span className='align-middle me-2'>Go to Page</span>
+            </div>
+            <input
+              type='number'
+              defaultValue={pageIndex + 1}
+              onChange={(e) => {
+                const page = e.target.value ? Number(e.target.value) - 1 : 0
+                gotoPage(page)
+              }}
+              className={['form-control', styles.goToPageInput].join(' ')}
+            />
           </div>
-          <input
-            type='number'
-            defaultValue={pageIndex + 1}
-            onChange={(e) => {
-              const page = e.target.value ? Number(e.target.value) - 1 : 0
-              gotoPage(page)
-            }}
-            className={['form-control', styles.goToPageInput].join(' ')}
-          />
         </div>
-        <div>
-          <button
-            className={['btn', styles.btnPagination].join(' ')}
-            onClick={previousPage}
-            disabled={!canPreviousPage || isLoading}
-          >
-            Previous
-          </button>
-        </div>
-        <div>
-          <button
-            className={['btn', styles.btnPagination].join(' ')}
-            onClick={nextPage}
-            disabled={!canNextPage || isLoading}
-          >
-            Next
-          </button>
+        <div className='col'>
+          <div className='row'>
+            <div className='col d-grid'>
+              <button
+                className='btn btn-primary btn-sm'
+                onClick={previousPage}
+                disabled={!canPreviousPage || isLoading}
+              >
+                Previous
+              </button>
+            </div>
+            <div className='col d-grid'>
+              <button
+                className='btn btn-primary btn-sm'
+                onClick={nextPage}
+                disabled={!canNextPage || isLoading}
+              >
+                Next
+              </button>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
